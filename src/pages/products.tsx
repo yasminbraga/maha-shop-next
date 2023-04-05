d4650982f739670c368d809c1760e5d0ded7d47d
yasminbraga <yasminbragateixeira@gmail.com>
2023-04-05 15:53:41 -0300

    first commit

---
+++ src/pages/products.tsx
@@ -0,0 +1,31 @@
+import { GetStaticProps } from "next";
+import React from "react";
+
+interface ProductType {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+
+  file: {
+    url: string;
+  };
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("http://127.0.0.1:3333/api/products");
+  const products = await response.json();
+  return { props: { products } };
+};
+
+const Products: React.FC = ({ products }) => {
+  return (
+    <div>
+      {products.map((product: ProductType) => (
+        <p key={product.id}>{product.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
---
+++ src/pages/signup.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signup: React.FC = () => {
+  return <h1>Criar conta</h1>;
+};
+
+export default Signup;
